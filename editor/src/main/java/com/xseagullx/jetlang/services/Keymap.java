44d7e47267773812352b02b2cf983fbbe7334090
Pavel Dionisev <xSeagullx@gmail.com>
2017-04-25 02:28:43 +0200

    Safer shortcut handling.

--- editor/src/main/java/com/xseagullx/jetlang/services/Keymap.java
+++ editor/src/main/java/com/xseagullx/jetlang/services/Keymap.java
@@ -1,26 +1,63 @@
 package com.xseagullx.jetlang.services;
 
 import java.awt.KeyboardFocusManager;
-import java.awt.event.KeyEvent;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+
+import static java.awt.event.KeyEvent.CTRL_MASK;
+import static java.awt.event.KeyEvent.KEY_RELEASED;
+import static java.awt.event.KeyEvent.META_MASK;
+import static java.awt.event.KeyEvent.VK_ENTER;
+import static java.awt.event.KeyEvent.VK_ESCAPE;
+import static java.awt.event.KeyEvent.VK_O;
+import static java.awt.event.KeyEvent.VK_Q;
+import static java.awt.event.KeyEvent.VK_S;
+import static java.awt.event.KeyEvent.VK_T;
+import static java.awt.event.KeyEvent.VK_W;
 
 public class Keymap {
+	private static class Shortcut {
+		private final int key;
+		private final int modifiers;
+
+		private Shortcut(int key, int modifiers) {
+			this.key = key;
+			this.modifiers = modifiers;
+		}
+
+		private Shortcut(int key) {
+			this.key = key;
+			this.modifiers = 0;
+		}
+
+		@Override public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			Shortcut shortcut = (Shortcut)o;
+			return key == shortcut.key &&
+				modifiers == shortcut.modifiers;
+		}
+
+		@Override public int hashCode() {
+			return Objects.hash(key, modifiers);
+		}
+	}
+
 	public static void register(ActionManager actionManager) {
-		Map<String, ActionManager.Action> keymap = new HashMap<>();
-		bind(keymap, ActionManager.Action.RUN, "⌘+⏎", "Ctrl+Enter");
-		bind(keymap, ActionManager.Action.OPEN, "⌘+O", "Ctrl+O");
-		bind(keymap, ActionManager.Action.SAVE, "⌘+S", "Ctrl+S");
-		bind(keymap, ActionManager.Action.QUIT, "⌘+Q", "Ctrl+Q");
-		bind(keymap, ActionManager.Action.CLOSE, "⌘+W", "Ctrl+W");
-		bind(keymap, ActionManager.Action.TOGGLE_SLOW_MO, "⌘+T", "Ctrl+T");
-		bind(keymap, ActionManager.Action.STOP, "⎋", "Escape");
+		Map<Shortcut, ActionManager.Action> keymap = new HashMap<>();
+		int oSSpecificModifier = "Mac OS X".equals(System.getProperty("os.name")) ? META_MASK : CTRL_MASK;
+		keymap.put(new Shortcut(VK_ENTER, oSSpecificModifier), ActionManager.Action.RUN);
+		keymap.put(new Shortcut(VK_O, oSSpecificModifier), ActionManager.Action.OPEN);
+		keymap.put(new Shortcut(VK_S, oSSpecificModifier), ActionManager.Action.SAVE);
+		keymap.put(new Shortcut(VK_Q, oSSpecificModifier), ActionManager.Action.QUIT);
+		keymap.put(new Shortcut(VK_W, oSSpecificModifier), ActionManager.Action.CLOSE);
+		keymap.put(new Shortcut(VK_T, oSSpecificModifier), ActionManager.Action.TOGGLE_SLOW_MO);
+		keymap.put(new Shortcut(VK_ESCAPE), ActionManager.Action.STOP);
 
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(e -> {
-			if (e.getID() == KeyEvent.KEY_RELEASED) {
-				String modifierText = KeyEvent.getKeyModifiersText(e.getModifiers());
-				String keyText = KeyEvent.getKeyText(e.getKeyCode());
-				String shortcut = ((!"".equals(modifierText)) ? modifierText + "+" : "") + keyText;
+			if (e.getID() == KEY_RELEASED) {
+				Shortcut shortcut = new Shortcut(e.getKeyCode(), e.getModifiers());
 				ActionManager.Action action = keymap.get(shortcut);
 				if (action != null) {
 					actionManager.fire(action);
@@ -29,10 +66,5 @@ public class Keymap {
 			}
 			return false;
 		});
-	}
-
-	private static void bind(Map<String, ActionManager.Action> keymap, ActionManager.Action action, String... shortcuts) {
-		for (String shortcut : shortcuts)
-			keymap.put(shortcut, action);
 	}
 }
